8abfee2a07787061c9659b3426ebb286c6ed82bf
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-03-08 23:46:47 +0100

    switch to cln_plugin's new ConfigOption

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use crate::config::read_config;
 use crate::util::{send_mail, send_telegram};
 
 use anyhow::anyhow;
-use cln_plugin::{options, Builder};
+use cln_plugin::options::{
+    BooleanConfigOption, ConfigOption, IntegerConfigOption, StringConfigOption,
+};
+use cln_plugin::Builder;
 
 use log::{debug, info, warn};
 use rpc::test_notifications;
@@ -17,6 +20,33 @@ mod rpc;
 mod structs;
 mod util;
 
+const OPT_AMBOSS: BooleanConfigOption =
+    ConfigOption::new_bool_no_default("vitality-amboss", "Switch on/off amboss");
+const OPT_EXPIRING_HTLCS: IntegerConfigOption = ConfigOption::new_i64_no_default(
+    "vitality-expiring-htlcs",
+    "Set block amount to watch for expiry",
+);
+const OPT_WATCH_CHANNELS: BooleanConfigOption =
+    ConfigOption::new_bool_no_default("vitality-watch-channels", "Switch on/off watch_channels");
+const OPT_WATCH_GOSSIP: BooleanConfigOption =
+    ConfigOption::new_bool_no_default("vitality-watch-gossip", "Switch on/off watch_gossip");
+const OPT_TELEGRAM_TOKEN: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-telegram-token", "Set telegram token");
+const OPT_TELEGRAM_USERNAMES: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-telegram-usernames", "Set telegram users");
+const OPT_SMTP_USERNAME: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-smtp-username", "Set smtp username");
+const OPT_SMTP_PASSWORD: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-smtp-password", "Set smtp password");
+const OPT_SMTP_SERVER: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-smtp-server", "Set smtp server");
+const OPT_SMTP_PORT: IntegerConfigOption =
+    ConfigOption::new_i64_no_default("vitality-smtp-port", "Set smtp port");
+const OPT_EMAIL_FROM: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-email-from", "Set email_from");
+const OPT_EMAIL_TO: StringConfigOption =
+    ConfigOption::new_str_no_default("vitality-email-to", "Set email_to");
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     std::env::set_var("CLN_PLUGIN_LOG", "cln_plugin=info,cln_rpc=info,debug");
@@ -24,66 +54,18 @@ async fn main() -> Result<(), anyhow::Error> {
     let state = PluginState::new();
     // let defaultconfig = Config::new();
     let confplugin = match Builder::new(tokio::io::stdin(), tokio::io::stdout())
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-amboss"),
-            options::Value::OptBoolean,
-            "Switch on/off amboss",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-expiring-htlcs"),
-            options::Value::OptInteger,
-            "Set block amount to watch for expiry",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-watch-channels"),
-            options::Value::OptBoolean,
-            "Switch on/off watch_channels",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-watch-gossip"),
-            options::Value::OptBoolean,
-            "Switch on/off watch_gossip",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-telegram-token"),
-            options::Value::OptString,
-            "Set telegram token",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-telegram-usernames"),
-            options::Value::OptString,
-            "Set telegram users",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-smtp-username"),
-            options::Value::OptString,
-            "Set smtp username",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-smtp-password"),
-            options::Value::OptString,
-            "Set smtp password",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-smtp-server"),
-            options::Value::OptString,
-            "Set smtp server",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-smtp-port"),
-            options::Value::OptInteger,
-            "Set smtp port",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-email-from"),
-            options::Value::OptString,
-            "Set email from",
-        ))
-        .option(options::ConfigOption::new(
-            &(PLUGIN_NAME.to_string() + "-email-to"),
-            options::Value::OptString,
-            "Set email to",
-        ))
+        .option(OPT_AMBOSS)
+        .option(OPT_EXPIRING_HTLCS)
+        .option(OPT_WATCH_CHANNELS)
+        .option(OPT_WATCH_GOSSIP)
+        .option(OPT_TELEGRAM_TOKEN)
+        .option(OPT_TELEGRAM_USERNAMES)
+        .option(OPT_SMTP_USERNAME)
+        .option(OPT_SMTP_PASSWORD)
+        .option(OPT_SMTP_SERVER)
+        .option(OPT_SMTP_PORT)
+        .option(OPT_EMAIL_FROM)
+        .option(OPT_EMAIL_TO)
         .rpcmethod(
             &(PLUGIN_NAME.to_string() + "-testnotifications"),
             "test notifications settings",
