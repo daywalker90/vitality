e80f182135bbf6eaa9452730da389ef782b6d848
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-03-27 18:47:25 +0100

    use jemalloc, as it keeps reserved memory usage low in long running programs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 extern crate serde_json;
 
 use crate::config::read_config;
