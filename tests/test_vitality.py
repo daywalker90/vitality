43f21fd990709d0b0fc56dfbd5d5434a9b17515b
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-03-28 22:46:38 +0100

    add tests

---
+++ tests/test_vitality.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+
+from pyln.testing.fixtures import *  # noqa: F403
+from util import get_plugin  # noqa: F401
+
+
+def test_basic(node_factory, get_plugin):  # noqa: F811
+    node = node_factory.get_node(options={"plugin": get_plugin})
---
+++ tests/util.py
@@ -0,0 +1,42 @@
+import logging
+import os
+import random
+import string
+from pathlib import Path
+
+import pytest
+
+RUST_PROFILE = os.environ.get("RUST_PROFILE", "debug")
+COMPILED_PATH = Path.cwd() / "target" / RUST_PROFILE / "vitality"
+DOWNLOAD_PATH = Path.cwd() / "tests" / "vitality"
+
+
+@pytest.fixture
+def get_plugin(directory):
+    if COMPILED_PATH.is_file():
+        return COMPILED_PATH
+    elif DOWNLOAD_PATH.is_file():
+        return DOWNLOAD_PATH
+    else:
+        raise ValueError("No files were found.")
+
+
+def generate_random_label():
+    label_length = 8
+    random_label = "".join(
+        random.choice(string.ascii_letters) for _ in range(label_length)
+    )
+    return random_label
+
+
+def generate_random_number():
+    return random.randint(1, 20_000_000_000_000_00_000)
+
+
+def pay_with_thread(rpc, bolt11):
+    LOGGER = logging.getLogger(__name__)
+    try:
+        rpc.dev_pay(bolt11, dev_use_shadow=False)
+    except Exception as e:
+        LOGGER.debug(f"holdinvoice: Error paying payment hash:{e}")
+        pass
